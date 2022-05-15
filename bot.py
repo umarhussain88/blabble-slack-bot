b33c19d8ec833f6746959fb4586977a48fb4961d
umar-desktop <umar.hussain.da@outlook.com>
2022-05-15 15:24:32 +0100

    changes to accomdate change in data types, django apps that affected sql table names and updating git ignore to include .vscode folder

--- bot.py
+++ bot.py
@@ -1,26 +1,32 @@
 import slack_sdk as slack
-import os 
+import os
 from src import blabble, logger_util
 from time import sleep
 
 
 logger = logger_util(__name__)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
 
 
-sql_user = os.environ.get('sql_user')
-sql_password = os.environ.get('sql_password')
-sql_host = os.environ.get('sql_host')
-sql_port = os.environ.get('sql_port')
+sql_user = os.environ.get("sql_user")
+sql_password = os.environ.get("sql_password")
+sql_host = os.environ.get("sql_host")
+sql_port = os.environ.get("sql_port")
+sql_db = os.environ.get("sql_db")
 
 lead = blabble.Lead(
-    sql_user,sql_password,sql_host,sql_port)
+    sql_user=sql_user,
+    sql_password=sql_password,
+    sql_host=sql_host,
+    sql_port=sql_port,
+    sql_db=sql_db,
+)
 
 
-with open('last_lead_id.csv', 'r') as f:
+with open("last_lead_id.csv", "r") as f:
     last_lead_id = int(f.read())
-    logger.info(f'Using  {last_lead_id} as the last lead_id from last_lead_id.csv')
+    logger.info(f"Using  {last_lead_id} as the last lead_id from last_lead_id.csv")
 
 lead_results = lead.return_leads(last_lead_id)
 
@@ -30,8 +36,10 @@ for result in lead_results:
         property_value = f"£{result.property_value:,.2f}"
     if result.mortgage_amount:
         mortgage_amount = f"£{result.mortgage_amount:,.2f}"
-    logger.info(f'Sending message to slack channel for lead_id: {result.lead_id}')
+    logger.info(f"Sending message to slack channel for lead_id: {result.lead_id}")
     sleep(1)
-    client.chat_postMessage(channel='#mortgage-leads', 
-            text = f""" New Lead: *#{result.lead_id} - {result.name}*\n*Property Value: {property_value}* *Loan Amount: {mortgage_amount}*  \n\nMessage: {result.message}\n<https://docs.google.com/forms/d/e/1FAIpQLSc6Xp_l9rfTA_OhW9wlHcjWMXyuGejOVoSJQeoo8eLuQqn2kA/viewform|Purchase Lead Here>
-                            """)
+    client.chat_postMessage(
+        channel="#mortgage-leads",
+        text=f""" New Lead: *#{result.lead_id} - {result.name}*\n*Property Value: {property_value}* *Loan Amount: {mortgage_amount}*  \n\nMessage: {result.message}\n<https://docs.google.com/forms/d/e/1FAIpQLSc6Xp_l9rfTA_OhW9wlHcjWMXyuGejOVoSJQeoo8eLuQqn2kA/viewform|Purchase Lead Here>
+                            """,
+    )
--- src/blabble.py
+++ src/blabble.py
@@ -11,6 +11,7 @@ from src import logger_util
 
 logger = logger_util(__name__)
 
+
 @dataclass
 class Engine:
 
@@ -18,14 +19,15 @@ class Engine:
     sql_password: str
     sql_host: str
     sql_port: str
+    sql_db: str
 
     def __post_init__(self):
         engine = create_engine(
-            f"postgresql://{self.sql_user}:{self.sql_password}@{self.sql_host}:{self.sql_port}/defaultdb"
+            f"postgresql://{self.sql_user}:{self.sql_password}@{self.sql_host}:{self.sql_port}/{self.sql_db}"
         )
 
-        #should add a check to check for engine connectivity 
-        #and raise an exception if not connected
+        # should add a check to check for engine connectivity
+        # and raise an exception if not connected
         logger.info(f"Connected to database: {engine.url}")
 
         object.__setattr__(self, "engine", engine)
@@ -38,58 +40,59 @@ class Lead(Engine):
 
     lead_id: int = 0
 
-    logger.info(f'Using  {lead_id} as the last lead_id')
+    logger.info(f"Using  {lead_id} as the last lead_id")
 
     lead = namedtuple(
         "lead",
-        "lead_id, property_value, mortgage_amount, form_submit_date, message, email, name, phone"
+        "lead_id, property_value, mortgage_amount, form_submit_date, message, email, name, phone",
     )
 
     def fetch_leads(self, lead_id: int) -> namedtuple:
 
         sql_str = f"""
-                    SELECT cl.id
-                        , COALESCE(cl.property_amount, 0)                      AS "Property Value"
-                        , COALESCE(cl.mortgage_amount, 0)                      AS "Loan Amount"
-                        , cl.create_date                                       AS "form submit date"
-                        , COALESCE(cl.message, '')
-                        , COALESCE(cc.email, '')                               AS email
-                        , CONCAT(cc.first_name, '', cc.last_name)              AS "Name"
-                        , mobile_phone                                         AS "Phone"
-                    FROM core_lead            cl
-                    LEFT JOIN core_customer cc
-                        ON cl.customer_id = cc.id
-                    WHERE cl.id > {lead_id}
-                    and cl.create_date >= '2022-04-04'
-                    and cc.email not like '%%test%%'
-                    ORDER BY cl.id ASC
+                    SELECT lead.id
+                         , CAST(COALESCE(NULLIF(lead.property_amount,''), '0') AS DECIMAL) AS "Property Value"
+                         , CAST(COALESCE(NULLIF(lead.mortgage_amount,''), '0') AS DECIMAL) AS "Loan Amount"
+                         , lead.created                                         AS "form submit date"
+                         , COALESCE(lead.message, '')
+                         , COALESCE(cust.email, '')                             AS email
+                         , CONCAT(cust.first_name, '', cust.last_name)          AS "Name"
+                         , mobile_phone                                         AS "Phone"
+                    FROM leads_lead            lead
+                      LEFT JOIN users_customer cust
+                        ON lead.customer_id = cust.id
+                    WHERE
+                      lead.id > 1296
+                    AND lead.created >= '2022-04-04'
+                    AND cust.email NOT LIKE '%%test%%'
+                    AND lead.message NOT LIKE '%%test%%'
+                    ORDER BY lead.id ASC
                     """
         res = self.engine.execute(sql_str)
 
         return res.fetchall()
 
-    def parse_leads(self, leads : list) -> List[namedtuple]:
+    def parse_leads(self, leads: list) -> List[namedtuple]:
 
         return [self.lead(*row) for row in leads]
 
     def return_leads(self, lead_id: int) -> List[namedtuple]:
         leads = self.fetch_leads(lead_id)
-        
-        if not leads:
-            logger.info(f'No leads found exiting')
-            sys.exit(0)
-            
 
-        logger.info(f'Found {len(leads)} leads')
-        #-1 = last record, 0 = lead_id.
+        if not leads:
+            logger.info(f"No leads found exiting")
+            sys.exit(0)
+
+        logger.info(f"Found {len(leads)} leads")
+        # -1 = last record, 0 = lead_id.
         self.store_lead_id(leads[-1][0])
-        
+
         return self.parse_leads(leads)
 
-    #locally store the last lead_id
+    # locally store the last lead_id
     def store_lead_id(self, lead_id: int) -> None:
 
-        logger.info(f'Storing {lead_id} as the max lead_id')
-        
-        with open('last_lead_id.csv', 'w') as f:
+        logger.info(f"Storing {lead_id} as the max lead_id")
+
+        with open("last_lead_id.csv", "w") as f:
             f.write(str(lead_id))
